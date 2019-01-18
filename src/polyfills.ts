326dce22f8fb47ba98260ee643fa808de66ba111
Michael DiCarlo <michael.v.dicarlo@gmail.com>
2019-01-18 15:44:30 -0500

    Started setting up Electron directories

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -77,6 +77,7 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
+import './zonejs-electron-fix.ts';
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
 
