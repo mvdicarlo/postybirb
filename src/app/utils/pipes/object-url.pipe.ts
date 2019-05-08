34898e3fcf99e97e42722116cf1444e894f4520e
Michael DiCarlo <michael.v.dicarlo@gmail.com>
2019-05-08 17:20:50 +0000

    Generic No Image Found placeholder added

--- src/app/utils/pipes/object-url.pipe.ts
+++ src/app/utils/pipes/object-url.pipe.ts
@@ -7,7 +7,7 @@ import { arrayBufferAsBlob } from '../helpers/file.helper';
 export class ObjectURLPipe implements PipeTransform {
 
   transform(blob: Blob, size: number = 100): any {
-    if (!blob) return '#';
+    if (!blob) return './assets/other-images/missing_img.png';
 
     const b: Blob = blob instanceof Uint8Array ? arrayBufferAsBlob(<Uint8Array>blob, 'image/jpeg') : blob
     // NOTE: size is deprecated for now since its only really used for blobs that have been resize
