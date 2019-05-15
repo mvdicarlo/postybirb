4c68e57cc029f076f2bcfd2b4ce985fffa014c2f
Michael DiCarlo <michael.v.dicarlo@gmail.com>
2019-05-15 11:28:37 +0000

    Start v2.1.9 Changed loading overlay for scheduled submissions

--- src/app/utils/components/loading-overlay/loading-overlay.component.ts
+++ src/app/utils/components/loading-overlay/loading-overlay.component.ts
@@ -3,11 +3,19 @@ import { Component, Input, Output, EventEmitter, ChangeDetectorRef, ChangeDetect
 @Component({
   selector: 'loading-overlay',
   templateUrl: './loading-overlay.component.html',
-  styleUrls: ['./loading-overlay.component.css'],
+  styleUrls: ['./loading-overlay.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoadingOverlay {
   @Output() readonly stop: EventEmitter<void> = new EventEmitter();
+
+  @Input()
+  get mode(): string { return this._mode }
+  set mode(loading: string) {
+    this._mode = loading;
+    this._changeDetector.detectChanges();
+  }
+  private _mode: string = 'buffer';
 
   @Input()
   get loading(): boolean { return this._loading }
@@ -24,6 +32,14 @@ export class LoadingOverlay {
     this._changeDetector.markForCheck();
   }
   private _allowStop: boolean = false;
+
+  @Input()
+  get faStopIcon(): string { return this._faStopIcon }
+  set faStopIcon(allowStop: string) {
+    this._faStopIcon = allowStop;
+    this._changeDetector.markForCheck();
+  }
+  private _faStopIcon: string;
 
   constructor(private _changeDetector: ChangeDetectorRef) { }
 
