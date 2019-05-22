1fc761c02497f71d2a7832adf841290d9424959e
Michael DiCarlo <michael.v.dicarlo@gmail.com>
2019-05-22 11:04:25 +0000

    Start v2.2.5 +Added progress bar while updating +NSIS should now allow directory to be specified

--- electron/src/auto-updater.js
+++ electron/src/auto-updater.js
@@ -1,5 +1,11 @@
-const { app, dialog } = require('electron');
-const { autoUpdater } = require('electron-updater');
+const {
+  app,
+  dialog,
+  BrowserWindow
+} = require('electron');
+const {
+  autoUpdater
+} = require('electron-updater');
 const log = require('electron-log');
 
 autoUpdater.logger = log;
@@ -8,6 +14,7 @@ autoUpdater.autoDownload = false;
 autoUpdater.allowPrerelease = true; // REMOVE WHEN 2.X GOES TO BEING MAIN BUILD!
 
 let updateDialogShowing = false;
+let progressWindow = null;
 
 // For details about these events, see the Wiki:
 // https://github.com/electron-userland/electron-builder/wiki/Auto-Update#events
@@ -19,40 +26,79 @@ let updateDialogShowing = false;
 //-------------------------------------------------------------------
 
 autoUpdater.on('checking-for-update', () => {
-    log.info('Checking for update...');
+  log.info('Checking for update...');
 });
 
 autoUpdater.on('update-available', (info) => {
-    if (!updateDialogShowing) {
-        let message = `${app.getName()} ${info.version} is now available.`;
-        if (info.releaseNotes) {
-            const splitNotes = info.releaseNotes.replace(/(<\w*>|<\/\w*>)/gm, '');
-            message += `\n\nRelease notes:\n${splitNotes}`;
-        }
-
-        dialog.showMessageBox({
-            type: 'question',
-            buttons: ['Install and Relaunch', 'Later'],
-            defaultId: 0,
-            message,
-        }, (response) => {
-            if (response === 0) {
-                setTimeout(() => autoUpdater.downloadUpdate(), 1);
-            }
-
-            updateDialogShowing = false;
-        });
-
-        updateDialogShowing = true;
+  if (!updateDialogShowing) {
+    let message = `${app.getName()} ${info.version} is now available.`;
+    if (info.releaseNotes) {
+      const splitNotes = info.releaseNotes.replace(/(<\w*>|<\/\w*>)/gm, '');
+      message += `\n\nRelease notes:\n${splitNotes}`;
     }
+
+    dialog.showMessageBox({
+      type: 'question',
+      buttons: ['Install and Relaunch', 'Later'],
+      defaultId: 0,
+      message,
+    }, (response) => {
+      if (response === 0) {
+        openProgressWindow();
+        setTimeout(() => autoUpdater.downloadUpdate(), 1);
+      }
+
+      updateDialogShowing = false;
+    });
+
+    updateDialogShowing = true;
+  }
 });
 
+autoUpdater.on('download-progress', (progressObj) => {
+  sendStatusToWindow(progressObj.percent);
+});
+
+
 autoUpdater.on('error', (err) => {
-    log.error(err);
+  log.error(err);
+  sendErrorToWindow(JSON.stringify(err));
 });
 
 autoUpdater.on('update-downloaded', (info) => {
-    setTimeout(() => autoUpdater.quitAndInstall(), 1);
+  if (progressWindow) {
+    progressWindow.close();
+    progressWindow = null;
+  }
+
+  setTimeout(() => autoUpdater.quitAndInstall(), 1);
 });
+
+function openProgressWindow() {
+  progressWindow = new BrowserWindow({
+    show: true,
+    width: 300,
+    height: 400,
+    resizable: false,
+    closable: false,
+    movable: true,
+    frame: false,
+    title: 'PostyBirb'
+  });
+
+  progressWindow.loadURL(`file://${__dirname}/html/update-progress.html`);
+}
+
+function sendErrorToWindow(error) {
+  if (progressWindow) {
+    progressWindow.webContents.send('error', error);
+  }
+}
+
+function sendStatusToWindow(text) {
+  if (progressWindow) {
+    progressWindow.webContents.send('message', text);
+  }
+}
 
 module.exports = autoUpdater;
