338bc3d9de9474d9d12950d92e3de42aa264affb
Michael DiCarlo <michael.v.dicarlo@gmail.com>
2019-05-26 15:01:44 -0400

    Attempt to make BBCode parsing better

--- src/app/utils/helpers/description-parsers/bbcode.parser.ts
+++ src/app/utils/helpers/description-parsers/bbcode.parser.ts
@@ -3,10 +3,6 @@ export class BBCodeParser {
     if (!html) return '';
 
     // html = html.replace(/<h[1-7](.*?)>(.*?)<\/h[1-7]>/, '\n[h]$2[/h]\n');
-    html = html.replace(/<h[1-7](.*?)>(.*?)<\/h[1-7]>/, '$2\n');
-
-    html = html.replace(/<br(.*?)>/gi, '\n');
-    html = html.replace(/<hr(.*?)>/gi, '\n[hr]\n');
 
     html = html.replace(/<b>/gi, '[b]');
     html = html.replace(/<i>/gi, '[i]');
@@ -24,22 +20,35 @@ export class BBCodeParser {
     const blocks = ['p', 'div', 'pre', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
     blocks.forEach(block => {
       const regex = new RegExp(`<${block}(.*?)style="text-align: ((left|right|center)?);"(.*?)>((.|\n)*?)<\/${block}>`, 'gmi');
-      html = html.replace(regex, '[$3]$5[/$3]\n');
+      html = html.replace(regex, '[$3]$5[/$3]');
+
+      const regex2 = new RegExp(`<${block}>\\s<\/${block}>`, 'gm');
+      html = html.replace(regex2, '<br>');
     });
+
+    html = BBCodeParser.postParse(html);
 
     html = html.replace(/ data-mce-(style)="(.*?);"/gmi, '');
     html = html.replace(/<span style="color: (.*?);">((.|\n)*?)<\/span>/gmi, '[color=$1]$2[/color]');
+
+    html = html.replace(/<h[1-7](.*?)>(.*?)<\/h[1-7]>/, '$2\n');
+
+    html = html.replace(/<br(.*?)>/gi, '\n');
+    html = html.replace(/<hr(.*?)>/gi, '\n[hr]\n');
 
     // opting to use \r for now
     html = html.replace(/<li(.*?)>(.*?)<\/li>/gi, '[*]$2');
     html = html.replace(/<ul(.*?)>/gi, '[list]');
     html = html.replace(/<\/ul>/gi, '[/list]');
-    html = html.replace(/<div>/gi, '');
-    html = html.replace(/<\/div>/gi, '\r');
-    html = html.replace(/<p>/gi, '');
-    html = html.replace(/<\/p>/gi, '\r');
-    html = html.replace(/<pre>/gi, '');
-    html = html.replace(/<\/pre>/gi, '\r');
+
+    blocks.forEach(block => {
+      const regex1 = new RegExp(`</${block}>`, 'gmi');
+      html = html.replace(regex1, '\n');
+
+      const regex2 = new RegExp(`<${block}>`, 'gmi');
+      html = html.replace(regex2, '');
+    });
+
     html = html.replace(/<td(.*?)>/gi, ' ');
     html = html.replace(/<tr(.*?)>/gi, '\r');
     html = html.replace(/<a(.*?)href="(.*?)"(.*?)>(.*?)<\/a>/gi, '[url=$2]$4[/url]');
@@ -53,12 +62,35 @@ export class BBCodeParser {
 
     html = html.replace(/<(?:[^>'"]*|(['"]).*?\1)*>/gmi, '');
     html = html.replace(/\r\r/gi, '');
-    html = html.replace(/(\S)\n/gi, '$1 ');
+    // html = html.replace(/(\S)\n/gi, '$1 ');
     html = html.replace(/&gt;/gi, '>');
     html = html.replace(/&lt;/gi, '<');
     html = html.replace(/&amp;/gi, '&');
 
-
     return html.trim();
+  }
+
+  private static postParse(html: string): string {
+    if (!html) return '';
+
+    html = html.replace(/(\n|\r)/g, '');
+
+    const tags: string[] = ['b','s','u','i'];
+    tags.forEach(tag => {
+      const regex = new RegExp(`\\[\/${tag}\\]<\/p><p>\\[${tag}\\]`, 'gmi');
+      html = html.replace(regex, '\n');
+    });
+
+    const blocks = ['left', 'right', 'center'];
+    blocks.forEach(block => {
+      const regex = new RegExp(`\\[\/${block}\\]\\[${block}\\]`, 'gmi');
+      html = html.replace(regex, '\n');
+    });
+
+    // html = html.replace(/\[\/center\]/gmi, '[/center]\r');
+    // html = html.replace(/\[\/right\]/gmi, '[/right]\r');
+    // html = html.replace(/\[\/left\]/gmi, '[/left]\r');
+
+    return html;
   }
 }
