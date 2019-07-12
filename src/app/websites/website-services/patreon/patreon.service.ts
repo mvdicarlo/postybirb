4869f591c6e916a02db209f15d127cb991cca7db
Michael DiCarlo <michael.v.dicarlo@gmail.com>
2019-07-12 12:51:09 +0000

    :Attempts at fixing patreon

--- src/app/websites/website-services/patreon/patreon.service.ts
+++ src/app/websites/website-services/patreon/patreon.service.ts
@@ -10,6 +10,7 @@ import { GenericJournalSubmissionForm } from '../../components/generic-journal-s
 import { SubmissionType } from 'src/app/database/tables/submission.table';
 import { TypeOfSubmission } from 'src/app/utils/enums/type-of-submission.enum';
 import { ISubmissionFileWithArray } from 'src/app/database/tables/submission-file.table';
+import { BrowserWindowHelper } from 'src/app/utils/helpers/browser-window.helper';
 
 function submissionValidate(submission: Submission, formData: SubmissionFormData): any[] {
   const problems: any[] = [];
@@ -188,32 +189,65 @@ export class Patreon extends BaseWebsiteService {
     return 'image_file';
   }
 
-  private async _uploadFile(link: string, file: ISubmissionFileWithArray, attachment: boolean, cookies: any[], csrf: string): Promise<any> {
+  private async _uploadFile(id: string, file: ISubmissionFileWithArray, attachment: boolean, cookies: any[], csrf: string, partitionId: string): Promise<any> {
     const uuid = URL.createObjectURL(fileAsBlob(file));
 
-    const fileData: any = {
-      qquuid: uuid,
-      qqfilename: file.fileInfo.name,
-      qqtotalfilesize: file.fileInfo.size,
-      file: fileAsFormDataObject(file)
+    const data: any = {
+      data: {
+        attributes: {
+          state: 'pending_upload',
+          owner_id: id,
+          owner_type: 'post',
+          owner_relationship: 'main',
+          file_name: file.fileInfo.name
+        },
+        type: 'media'
+      }
     };
 
-    const upload = await got.post(`${link}/${attachment ? 'attachments' : 'post_file'}?json-api-version=1.0`, fileData, this.BASE_URL, cookies, {
-      headers: {
-        'X-CSRF-Signature': csrf,
-        'X-Requested-With': 'XMLHttpRequest'
+    const idk = await got.get(`${this.BASE_URL}/REST/auth/CSRFTicket?uri=${encodeURIComponent(`"/posts/${id}/edit&json-api-version=1.0`)}`, this.BASE_URL, cookies, partitionId);
+
+    const upload = await got.crPost(`${this.BASE_URL}/api/media?json-api-version=1.0`, {
+      'X-CSRF-Signature': csrf,
+      'Host': 'www.patreon.com',
+      'Referer': `https://www.patreon.com/posts/${id}/edit`,
+      'Cookie': cookies.map(c => `${c.name}=${c.value}`).join('; '),
+      'Cache-Control': 'no-cache',
+      'Accept': '*/*',
+      'Pragma': 'no-cache',
+    }, partitionId, data, true);
+
+    try {
+      const body: any = JSON.parse(upload.body);
+      const amazonData = body.data.attributes.upload_parameters;
+      amazonData.file = fileAsFormDataObject(file);
+
+      const sendFile = await got.crPost(body.data.attributes.upload_url, {
+        'Accept': '*/*',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Host': 'patreon-media.s3-accelerate.amazonaws.com',
+        'Origin': 'https://www.patreon.com',
+        'Referer': 'https://www.patreon.com/',
+        'Connection': 'keep-alive',
+        'Cache-Control': 'no-cache',
+        'Pragma': 'no-cache',
+        'Keep-Alive': 300,
+        'Access-Control-Request-Method': 'POST'
+      }, partitionId, amazonData, false);
+
+      if (sendFile.body.includes('Error')) {
+        return Promise.reject(this.createPostResponse('Problem uploading file', sendFile.body));
       }
-    });
 
-    if (upload.error) {
-      return Promise.reject(this.createPostResponse('Problem uploading file', upload.error));
+      return body;
+    } catch (err) {
+      return Promise.reject(this.createPostResponse('Problem uploading file', upload.body));
     }
-
-    return upload.success.body;
   }
 
   private async postSubmission(submission: Submission, postData: SubmissionPostData): Promise<PostResult> {
-    const cookies = await getCookies(postData.profileId, this.BASE_URL);
+    let cookies = await getCookies(postData.profileId, this.BASE_URL);
     const csrf = await this._getCSRF(cookies, postData.profileId);
 
     const createData = {
@@ -225,28 +259,38 @@ export class Patreon extends BaseWebsiteService {
       }
     };
 
-    const create = await got.post(`${this.BASE_URL}/api/posts?json-api-version=1.0`, null, this.BASE_URL, cookies, {
-      json: createData,
-      headers: {
-        'X-CSRF-Signature': csrf
-      }
-    });
+    await BrowserWindowHelper.hitUrl(postData.profileId, 'https://www.patreon.com/posts/new?ru=%2Fhome');
+    cookies = await getCookies(postData.profileId, this.BASE_URL)
 
-    if (create.error) {
-      return Promise.reject(this.createPostResponse('Unknown error', create.error));
+    const create = await got.crPost(`${this.BASE_URL}/api/posts?fields[post]=post_type%2Cpost_metadata&json-api-version=1.0&include=[]`, {
+            'Accept': '*/*',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive',
+            'Content-Type': 'application/vnd.api+json',
+            'Host': 'www.patreon.com',
+            'Origin': 'https://www.patreon.com',
+            'Pragma': 'no-cache',
+            'Referer': 'https://www.patreon.com/posts/new',
+            'X-CSRF-Signature': csrf,
+            'Cookie': cookies.map(c => `${c.name}=${c.value}`).join('; ')
+          }, postData.profileId, createData, true);
+
+    if (!create.body || !create.body.includes('self')) {
+      return Promise.reject(this.createPostResponse('Unknown error', create.body));
     }
 
-    const response: any = create.success.body;
-    const link = response.links.self;
+    const response: any = JSON.parse(create.body);
+    const link = response.data.id;
 
-    const primaryFileUpload = await this._uploadFile(link, postData.primary, postData.typeOfSubmission === TypeOfSubmission.STORY, cookies, csrf);
+    const primaryFileUpload = await this._uploadFile(link, postData.primary, postData.typeOfSubmission === TypeOfSubmission.STORY, cookies, csrf, postData.profileId);
     let uploadJSON: any = JSON.stringify(primaryFileUpload);
     if (uploadJSON.errors && uploadJSON.errors) {
       return Promise.reject(this.createPostResponse(uploadJSON.errors.map(err => `${err.code_name}: ${err.detail}\n`), uploadJSON.errors));
     }
 
     for (let i = 0; i < postData.additionalFiles.length; i++) {
-      const upload = await this._uploadFile(link, postData.additionalFiles[i], true, cookies, csrf);
+      const upload = await this._uploadFile(link, postData.additionalFiles[i], true, cookies, csrf, postData.profileId);
       uploadJSON = JSON.stringify(upload);
       if (uploadJSON.errors && uploadJSON.errors) {
         return Promise.reject(this.createPostResponse(uploadJSON.errors.map(err => `${err.code_name}: ${err.detail}\n`), uploadJSON.errors));
@@ -298,7 +342,11 @@ export class Patreon extends BaseWebsiteService {
     const postResponse = await got.patch(`${link}?json-api-version=1.0`, null, this.BASE_URL, cookies, {
       json: data,
       headers: {
-        'X-CSRF-Signature': csrf
+        'X-CSRF-Signature': csrf,
+        'Host': 'www.patreon.com',
+        'Referer': 'https://www.patreon.com/posts',
+        'Origin': 'https://www.patreon.com',
+        'Accept': '*/*'
       }
     });
 
