b47a368c9beccf0ccd57b54a852f4573054f6942
Michael DiCarlo <michael.v.dicarlo@gmail.com>
2019-07-24 10:50:01 +0000

    Patreon access tier validation is better

--- src/app/websites/website-services/patreon/components/patreon-submission-form/patreon-submission-form.component.ts
+++ src/app/websites/website-services/patreon/components/patreon-submission-form/patreon-submission-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, forwardRef, Injector, AfterViewInit } from '@angular
 import { BaseWebsiteSubmissionForm, HOST_DATA } from 'src/app/websites/components/base-website-submission-form/base-website-submission-form.component';
 import { TagConfig } from 'src/app/utils/components/tag-input/tag-input.component';
 import { Folder } from 'src/app/websites/interfaces/folder.interface';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'patreon-submission-form',
@@ -13,7 +14,7 @@ import { Folder } from 'src/app/websites/interfaces/folder.interface';
 export class PatreonSubmissionForm extends BaseWebsiteSubmissionForm implements OnInit, AfterViewInit {
 
   public optionDefaults: any = {
-    tiers: [[]],
+    tiers: [[], Validators.minLength(1)],
     chargePatrons: [false],
     schedule: [null]
   };
@@ -39,6 +40,23 @@ export class PatreonSubmissionForm extends BaseWebsiteSubmissionForm implements 
     if (this.folders) {
       this.resetOnConflict('folder', this.getIdsFromFolders(this.folders));
     }
+
+    this.formGroup.controls.options.get('tiers').valueChanges.subscribe(tiers => {
+      if (tiers && tiers.length > 1) {
+        let exclusive: any[] = [];
+        let nonExclusive: any[] = [];
+        tiers.forEach(tier => {
+          const found = this.folders.find(t => t.id === tier);
+          if (found) exclusive.push(tier);
+          else nonExclusive.push(tier);
+        });
+
+        if (exclusive.length && nonExclusive.length || exclusive.length > 1) {
+          const keep = exclusive[exclusive.length - 1];
+          this.formGroup.controls.options.get('tiers').patchValue([keep]);
+        }
+      }
+    });
   }
 
 }
