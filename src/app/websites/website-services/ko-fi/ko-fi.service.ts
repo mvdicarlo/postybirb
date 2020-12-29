aee8c8c2c1d23646194ef6c5da07bf355c7f6645
Michael DiCarlo <michael.v.dicarlo@gmail.com>
2020-12-29 09:41:33 -0500

    Ko-fi now supports gold features and albums

--- src/app/websites/website-services/ko-fi/ko-fi.service.ts
+++ src/app/websites/website-services/ko-fi/ko-fi.service.ts
@@ -7,10 +7,16 @@ import { PlaintextParser } from 'src/app/utils/helpers/description-parsers/plain
 import { supportsFileType } from '../../helpers/website-validator.helper';
 import { SubmissionRating, SubmissionType } from 'src/app/database/tables/submission.table';
 import { Submission, SubmissionFormData } from 'src/app/database/models/submission.model';
-import { LoginStatus, WebsiteStatus, SubmissionPostData, PostResult } from '../../interfaces/website-service.interface';
+import {
+  LoginStatus,
+  WebsiteStatus,
+  SubmissionPostData,
+  PostResult,
+} from '../../interfaces/website-service.interface';
 import { KoFiSubmissionForm } from './components/ko-fi-submission-form/ko-fi-submission-form.component';
 import { fileAsFormDataObject } from 'src/app/utils/helpers/file.helper';
 import { HTMLParser } from 'src/app/utils/helpers/html-parser.helper';
+import { Folder } from '../../interfaces/folder.interface';
 
 const ACCEPTED_FILES = ['jpeg', 'jpg', 'png'];
 
@@ -22,31 +28,34 @@ function submissionValidate(submission: Submission, formData: SubmissionFormData
   }
 
   if (!supportsFileType(submission.fileInfo, ACCEPTED_FILES)) {
-    problems.push(['Does not support file format', { website: 'Ko-fi', value: submission.fileInfo.type }]);
+    problems.push([
+      'Does not support file format',
+      { website: 'Ko-fi', value: submission.fileInfo.type },
+    ]);
   }
 
   return problems;
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 @Website({
   acceptedFiles: ACCEPTED_FILES,
   displayedName: 'Ko-fi',
   login: {
-    url: 'https://ko-fi.com/account/login'
+    url: 'https://ko-fi.com/account/login',
   },
   components: {
     submissionForm: KoFiSubmissionForm,
-    journalForm: KoFiSubmissionForm
+    journalForm: KoFiSubmissionForm,
   },
   validators: {
-    submission: submissionValidate
+    submission: submissionValidate,
   },
   parsers: {
     description: [],
-  }
+  },
 })
 export class KoFi extends BaseWebsiteService {
   readonly BASE_URL: string = 'https://ko-fi.com';
@@ -58,7 +67,7 @@ export class KoFi extends BaseWebsiteService {
   public async checkStatus(profileId: string): Promise<WebsiteStatus> {
     const returnValue: WebsiteStatus = {
       username: null,
-      status: LoginStatus.LOGGED_OUT
+      status: LoginStatus.LOGGED_OUT,
     };
 
     const cookies = await getCookies(profileId, this.BASE_URL);
@@ -68,13 +77,50 @@ export class KoFi extends BaseWebsiteService {
         returnValue.status = LoginStatus.LOGGED_IN;
         returnValue.username = HTMLParser.getInputValue(response.body, 'DisplayName');
         this.userInformation.set(profileId, {
-          id: response.body.match(/pageId:\s'(.*?)'/)[1]
+          id: response.body.match(/pageId:\s'(.*?)'/)[1],
         });
 
+        await this.loadAlbums(profileId, cookies, response.body.match(/buttonId:\s'(.*?)'/)[1]);
       }
-    } catch (e) { /* No important error handling */ }
+    } catch (e) {
+      /* No important error handling */
+    }
 
     return returnValue;
+  }
+
+  private async loadAlbums(profileId: string, cookies: any[], id: string): Promise<void> {
+    const response = await got.get(
+      `${this.BASE_URL}/${id}/gallery`,
+      this.BASE_URL,
+      cookies,
+      undefined
+    );
+    const albums: Folder[] = [
+      {
+        title: 'No album',
+        id: '',
+      },
+    ];
+
+    $(response.body)
+      .find('.hz-album-each')
+      .each(function () {
+        const title = this.innerText.trim();
+        if (title !== 'New') {
+          albums.push({
+            title,
+            id: this.firstElementChild.href.split('/').pop(),
+          });
+        }
+      });
+
+    this.storeUserInformation(profileId, 'folders', albums);
+  }
+
+  public getFolders(profileId: string): Folder[] {
+    const info = this.userInformation.get(profileId);
+    return info.folders || [];
   }
 
   public post(submission: Submission, postData: SubmissionPostData): Promise<PostResult> {
@@ -87,7 +133,10 @@ export class KoFi extends BaseWebsiteService {
     }
   }
 
-  private async postJournal(submission: Submission, postData: SubmissionPostData): Promise<PostResult> {
+  private async postJournal(
+    submission: Submission,
+    postData: SubmissionPostData
+  ): Promise<PostResult> {
     const cookies = await getCookies(postData.profileId, this.BASE_URL);
 
     const data = {
@@ -100,37 +149,45 @@ export class KoFi extends BaseWebsiteService {
       showFeaturedImageOnPost: 'true',
       embedUrl: '',
       tags: this.formatTags(postData.tags, []).join(','),
-      postAudience: postData.options.audience || 'public'
+      postAudience: postData.options.audience || 'public',
     };
 
-    const postResponse = await got.post(`${this.BASE_URL}/Blog/AddBlogPost`, data, this.BASE_URL, cookies, {
-      headers: {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;',
-        'Pragma': 'no-cache',
-        'Cache-Control': 'no-cache',
-        'Referer': 'https://ko-fi.com/',
-        'Connection': 'keep-alive'
+    const postResponse = await got.post(
+      `${this.BASE_URL}/Blog/AddBlogPost`,
+      data,
+      this.BASE_URL,
+      cookies,
+      {
+        headers: {
+          Accept: 'text/html,application/xhtml+xml,application/xml;',
+          Pragma: 'no-cache',
+          'Cache-Control': 'no-cache',
+          Referer: 'https://ko-fi.com/',
+          Connection: 'keep-alive',
+        },
       }
-    });
+    );
     if (postResponse.error) {
       return Promise.reject(this.createPostResponse('Unknown error', postResponse.error));
     }
 
     const postUrl = (postResponse.success.body.match(/\/Blog\/PublishPost\/\d+/) || [])[0];
     if (!postUrl) {
-      return Promise.reject(this.createPostResponse('Unable to detect a posting Url', postResponse.success.body));
+      return Promise.reject(
+        this.createPostResponse('Unable to detect a posting Url', postResponse.success.body)
+      );
     }
 
     const publish = await got.post(`${this.BASE_URL}${postUrl}`, null, this.BASE_URL, cookies, {
       gzip: true,
       headers: {
         'Accept-Encoding': 'gzip, deflate, br',
-        'Accept': '*/*',
-        'Pragma': 'no-cache',
+        Accept: '*/*',
+        Pragma: 'no-cache',
         'Cache-Control': 'no-cache',
-        'Referer': 'https://ko-fi.com/',
-        'Connection': 'keep-alive'
-      }
+        Referer: 'https://ko-fi.com/',
+        Connection: 'keep-alive',
+      },
     });
     if (publish.error) {
       return Promise.reject(this.createPostResponse('Unknown error', publish.error));
@@ -143,19 +200,28 @@ export class KoFi extends BaseWebsiteService {
     }
   }
 
-  private async postSubmission(submission: Submission, postData: SubmissionPostData): Promise<PostResult> {
+  private async postSubmission(
+    submission: Submission,
+    postData: SubmissionPostData
+  ): Promise<PostResult> {
     const cookies = await getCookies(postData.profileId, this.BASE_URL);
 
     const uploadData: any = {
       uniqueFilename: '',
-      file: fileAsFormDataObject(postData.primary)
+      file: fileAsFormDataObject(postData.primary),
     };
 
-    const upload = await got.post(`${this.BASE_URL}/api/media/gallery-item/upload`, uploadData, this.BASE_URL, cookies, {
-      headers: {
-        'Referer': 'https://ko-fi.com/',
+    const upload = await got.post(
+      `${this.BASE_URL}/api/media/gallery-item/upload`,
+      uploadData,
+      this.BASE_URL,
+      cookies,
+      {
+        headers: {
+          Referer: 'https://ko-fi.com/',
+        },
       }
-    });
+    );
 
     if (upload.error) {
       return Promise.reject(this.createPostResponse('Unknown error', upload.error));
@@ -165,28 +231,42 @@ export class KoFi extends BaseWebsiteService {
     try {
       body = JSON.parse(upload.success.body);
     } catch (err) {
-      return Promise.reject(this.createPostResponse('Unknown error', `JSON PARSE FAILURE\n${upload.success.body}`))
+      return Promise.reject(
+        this.createPostResponse('Unknown error', `JSON PARSE FAILURE\n${upload.success.body}`)
+      );
     }
 
     const info = this.userInformation.get(postData.profileId);
 
     const data: any = {
-      Album: '',
+      Album: postData.options.folder || '',
+      Audience: postData.options.audience,
       Title: postData.title,
-      Description: PlaintextParser.parse(postData.description.replace('<p><a href="http://www.postybirb.com">Posted using PostyBirb</a></p>', '\n\nPosted using PostyBirb')),
+      Description: PlaintextParser.parse(
+        postData.description.replace(
+          '<p><a href="http://www.postybirb.com">Posted using PostyBirb</a></p>',
+          '\n\nPosted using PostyBirb'
+        )
+      ),
       PostToTwitter: 'false',
-      EnableHiRes: 'false',
+      EnableHiRes: postData.options.hiRes ? 'true' : 'false',
       ImageUploadIds: [body[0].ExternalId],
     };
 
-    const postResponse = await got.post(`${this.BASE_URL}/Feed/AddImageFeedItem`, data, this.BASE_URL, cookies, {
-      gzip: true,
-      headers: {
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept': 'text/html, */*',
-        'Referer': 'https://ko-fi.com/',
+    const postResponse = await got.post(
+      `${this.BASE_URL}/Feed/AddImageFeedItem`,
+      data,
+      this.BASE_URL,
+      cookies,
+      {
+        gzip: true,
+        headers: {
+          'Accept-Encoding': 'gzip, deflate, br',
+          Accept: 'text/html, */*',
+          Referer: 'https://ko-fi.com/',
+        },
       }
-    });
+    );
 
     if (postResponse.error) {
       return Promise.reject(this.createPostResponse('Unknown error', postResponse.error));
