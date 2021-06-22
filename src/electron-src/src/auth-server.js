0413249df994623f348254962d0c78e9ca930a9f
Michael DiCarlo <michael.v.dicarlo@gmail.com>
2021-06-22 08:07:41 -0400

    Auth: Changed auth to Microsoft Azure

--- src/electron-src/src/auth-server.js
+++ src/electron-src/src/auth-server.js
@@ -1,5 +1,5 @@
 module.exports = {
-  auth_server: 'https://postybirb-auth.cleverapps.io',
+  auth_server: 'https://postybirb-auth.azurewebsites.net',
 
   generateAuthUrl(path) {
     return this.auth_server + path;
