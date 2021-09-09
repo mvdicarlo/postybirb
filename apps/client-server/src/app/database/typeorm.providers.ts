e2bbea2fa607af3e6022be7c3f88a3542ebac632
Michael DiCarlo <michael.v.dicarlo@gmail.com>
2021-09-09 08:29:33 -0400

    Have TypeORM create database if it doesn't already exist

--- apps/client-server/src/app/database/typeorm.providers.ts
+++ apps/client-server/src/app/database/typeorm.providers.ts
@@ -2,15 +2,26 @@ import { Provider } from '@nestjs/common';
 import { createConnection } from 'typeorm';
 import { PostyBirbDirectories } from '@postybirb/fs';
 import { join } from 'path';
+import { existsSync } from 'fs';
 import { DATABASE_CONNECTION } from '../constants';
 
+// Only need to run a synchronize on first initialization.
+// Any schema changes afterwards should be handled by migrations.
+const DATABASE_PATH = join(PostyBirbDirectories.DATA_DIRECTORY, 'database.sql');
+const exists = existsSync(DATABASE_PATH)
+
+/*
+* TypeORM Database Provider
+* https://docs.nestjs.com/recipes/sql-typeorm
+*/
 export const typeormDatabaseProviders: Provider[] = [
   {
     provide: DATABASE_CONNECTION,
     useFactory: async () =>
       await createConnection({
         type: 'sqlite',
-        database: join(PostyBirbDirectories.DATA_DIRECTORY, 'database.sql'),
+        database: DATABASE_PATH,
+        synchronize: !exists
       }),
   },
 ];
