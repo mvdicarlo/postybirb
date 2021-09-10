4207bc0646698f20ec2e5338b5e141c3c4006b69
Michael DiCarlo <michael.v.dicarlo@gmail.com>
2021-09-10 06:49:13 -0400

    ClientServer: Added compression library

--- apps/client-server/src/main.ts
+++ apps/client-server/src/main.ts
@@ -1,10 +1,6 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as compression from 'compression';
 
 import { PostyBirbDirectories } from '@postybirb/fs';
 
@@ -14,8 +10,12 @@ async function bootstrap(appPort?: number) {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || appPort || 3333;
+  app.use(compression());
+
+  // PostyBirb Initialization
   PostyBirbDirectories.initializeDirectories();
+
+  const port = process.env.PORT || appPort || 3333;
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
   });
