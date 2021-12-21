dc6ae4b4dec89e08d0a6537e0f5b66d79a4cb629
Michael DiCarlo <michael.v.dicarlo@gmail.com>
2021-12-21 07:23:10 -0500

    change: Began setup of electron main setup configuration

--- apps/postybirb/src/main.ts
+++ apps/postybirb/src/main.ts
@@ -1,9 +1,43 @@
 import SquirrelEvents from './app/events/squirrel.events';
 import ElectronEvents from './app/events/electron.events';
 import UpdateEvents from './app/events/update.events';
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, powerSaveBlocker } from 'electron';
 import App from './app/app';
 import { bootstrapClientServer } from 'apps/client-server/src/main';
+
+const isOnlyInstance = app.requestSingleInstanceLock();
+if (!isOnlyInstance) {
+  app.quit();
+  process.exit();
+}
+
+app.commandLine.appendSwitch('disable-renderer-backgrounding');
+app.commandLine.appendSwitch('disable-background-timer-throttling');
+app.commandLine.appendSwitch('disable-features', 'CrossOriginOpenerPolicy');
+
+const psbId = powerSaveBlocker.start('prevent-app-suspension');
+
+app.on(
+  'certificate-error',
+  (
+    event: Electron.Event,
+    webContents: Electron.WebContents,
+    url: string,
+    error: string,
+    certificate: Electron.Certificate,
+    callback: (allow: boolean) => void
+  ) => {
+    if (
+      certificate.issuerName === 'postybirb.com' &&
+      certificate.subject.organizations[0] === 'PostyBirb' &&
+      certificate.issuer.country === 'US'
+    ) {
+      callback(true);
+    } else {
+      callback(false);
+    }
+  }
+);
 
 export default class Main {
   static initialize() {
