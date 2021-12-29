2b026cd8713f2fa6fa8ebd53887067608aa72cf2
Michael DiCarlo <michael.v.dicarlo@gmail.com>
2021-12-29 07:01:18 -0500

    initial render setup

--- apps/ui/src/app/app-layout.tsx
+++ apps/ui/src/app/app-layout.tsx
@@ -1,22 +1,40 @@
+import './app.css';
 import {
   EuiPage,
   EuiPageContent,
   EuiPageContentBody,
   EuiPageSideBar,
+  EuiHeader,
+  EuiHeaderLogo,
 } from '@elastic/eui';
 
 export default function AppLayout() {
   return (
-    <EuiPage paddingSize="none" className="w-screen h-screen">
-      <EuiPageSideBar sticky></EuiPageSideBar>
-      <EuiPageContent
-        hasBorder={false}
-        hasShadow={false}
-        paddingSize="none"
-        borderRadius="none"
-      >
-        <EuiPageContentBody restrictWidth></EuiPageContentBody>
-      </EuiPageContent>
-    </EuiPage>
+    <>
+      <EuiHeader>
+        <EuiHeaderLogo
+          iconTitle="PostyBirb"
+          iconType={() => (
+            <img
+              className="euiIcon euiIcon--large euiHeaderLogo__icon"
+              src="/assets/app-icon.png"
+            />
+          )}
+        >
+          PostyBirb
+        </EuiHeaderLogo>
+      </EuiHeader>
+      <EuiPage paddingSize="none">
+        <EuiPageSideBar sticky></EuiPageSideBar>
+        <EuiPageContent
+          hasBorder={false}
+          hasShadow={false}
+          paddingSize="none"
+          borderRadius="none"
+        >
+          <EuiPageContentBody restrictWidth></EuiPageContentBody>
+        </EuiPageContent>
+      </EuiPage>
+    </>
   );
 }
