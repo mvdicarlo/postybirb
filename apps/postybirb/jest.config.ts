580c26eaf4b8891f5e53c39e7f4a26d170044c1d
Michael DiCarlo <michael.v.dicarlo@gmail.com>
2023-10-27 07:04:52 -0400

    chore: update to nx v15

--- apps/postybirb/jest.config.ts
+++ apps/postybirb/jest.config.ts
@@ -2,13 +2,14 @@
 export default {
   displayName: 'postybirb',
   preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsConfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
+  globals: {},
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsConfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/apps/postybirb',
