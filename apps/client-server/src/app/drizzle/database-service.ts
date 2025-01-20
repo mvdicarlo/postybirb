b7733b54472251331b3649ea0b707b6e216a6fe8
Michael DiCarlo <michael.v.dicarlo@gmail.com>
2025-01-20 16:18:58 -0500

    feat: subscriber added into service class

--- apps/client-server/src/app/drizzle/database-service.ts
+++ apps/client-server/src/app/drizzle/database-service.ts
@@ -10,7 +10,7 @@ import * as schema from './schemas';
 
 export type DrizzleDatabase = BetterSQLite3Database<typeof schema>;
 
-type SchemaKey = keyof typeof schema;
+type SchemaKey = keyof DrizzleDatabase['_']['schema'];
 type EntityConverter<TEntityClass extends DatabaseEntity> = (
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   value: any,
@@ -23,20 +23,57 @@ type Select<TSchemaKey extends SchemaKey> =
   (typeof schema)[TSchemaKey]['$inferSelect'];
 
 type ExtractedRelations = ExtractTablesWithRelations<typeof schema>;
+
 type Relation<TSchemaKey extends SchemaKey> =
-  TSchemaKey extends keyof ExtractedRelations
-    ? ExtractedRelations[TSchemaKey]
-    : never;
+  DrizzleDatabase['_']['schema'][TSchemaKey];
+
+export type Action = 'delete' | 'insert' | 'update';
 
 export class DatabaseService<
   TSchemaKey extends SchemaKey,
   TEntityClass extends DatabaseEntity,
 > {
+  private static readonly subscribers: Record<
+    SchemaKey,
+    Array<(ids: EntityId[], action: Action) => void>
+  > = {
+    account: [],
+    directoryWatcher: [],
+    fileBuffer: [],
+    postQueueRecord: [],
+    postRecord: [],
+    settings: [],
+    submissionFile: [],
+    submission: [],
+    tagConverter: [],
+    tagGroup: [],
+    userSpecifiedWebsiteOptions: [],
+    websiteData: [],
+    websiteOptions: [],
+    websitePostRecord: [],
+  };
+
   constructor(
     public readonly db: DrizzleDatabase,
     private readonly schemaKey: TSchemaKey,
     private readonly classConverter: EntityConverter<TEntityClass>,
   ) {}
+
+  public static subscribe(
+    key: SchemaKey,
+    callback: (ids: EntityId[], action: Action) => void,
+  ) {
+    if (!DatabaseService.subscribers[key]) {
+      DatabaseService.subscribers[key] = [];
+    }
+    DatabaseService.subscribers[key].push(callback);
+  }
+
+  private notify(ids: EntityId[], action: Action) {
+    DatabaseService.subscribers[this.schemaKey].forEach((callback) =>
+      callback(ids, action),
+    );
+  }
 
   private get schemaEntity() {
     return schema[this.schemaKey];
@@ -51,13 +88,19 @@ export class DatabaseService<
       .insert(this.schemaEntity)
       .values(value)
       .returning();
+    this.notify(
+      inserts.map((insert) => insert.id),
+      'insert',
+    );
     return this.classConverter(inserts.length > 1 ? inserts : inserts[0]);
   }
 
-  public deleteById(id: EntityId[]) {
-    return this.db
+  public async deleteById(ids: EntityId[]) {
+    const result = await this.db
       .delete(this.schemaEntity)
-      .where(eq(this.schemaEntity.id, id));
+      .where(eq(this.schemaEntity.id, ids));
+    this.notify(ids, 'delete');
+    return result;
   }
 
   public async findById(id: EntityId, options?: { failIfNotFound: boolean }) {
@@ -87,10 +130,7 @@ export class DatabaseService<
       DBQueryConfig<'many', true, ExtractedRelations, Relation<TSchemaKey>>
     >,
   ) {
-    const record =
-      (await this.db.query[this.schemaKey as keyof DrizzleDatabase].findMany(
-        query,
-      )) ?? [];
+    const record = (await this.db.query[this.schemaKey].findMany(query)) ?? [];
     return this.classConverter(record);
   }
 
@@ -123,9 +163,10 @@ export class DatabaseService<
   }
 
   public async update(id: EntityId, set: Partial<Select<TSchemaKey>>) {
-    return this.db
+    await this.db
       .update(this.schemaEntity)
       .set(set)
       .where(eq(this.schemaEntity.id, id));
+    this.notify([id], 'update');
   }
 }
