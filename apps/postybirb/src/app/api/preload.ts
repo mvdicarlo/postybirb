bfe57b354fbf97c3f794c1dc960cda7d115d1a4f
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2025-04-29 14:36:40 +0300

    fix: app crash when trying to open empty link

--- apps/postybirb/src/app/api/preload.ts
+++ apps/postybirb/src/app/api/preload.ts
@@ -3,8 +3,14 @@ import { contextBridge, ipcRenderer } from 'electron';
 contextBridge.exposeInMainWorld('electron', {
   getAppVersion: () => ipcRenderer.invoke('get-app-version'),
   pickDirectory: () => ipcRenderer.invoke('pick-directory'),
-  openExternalLink: (url: string) =>
-    ipcRenderer.send('open-external-link', url),
+  openExternalLink: (url: string) => {
+    // Prevent app crash from trying to open undefined link
+    if (!url) {
+      throw new TypeError(`openExternalLink: url cannot be empty! Got: ${url}`);
+    }
+
+    ipcRenderer.send('open-external-link', url);
+  },
   platform: process.platform,
   app_port: process.env.POSTYBIRB_PORT,
   app_version: process.env.POSTYBIRB_VERSION,
