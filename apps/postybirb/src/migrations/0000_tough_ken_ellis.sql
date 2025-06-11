166fe23835214e1ce84e7ff2ea05996a0537a5c2
Michael DiCarlo <michael.v.dicarlo@gmail.com>
2025-06-11 08:19:30 -0400

    release: v4.0.2

--- apps/postybirb/src/migrations/0000_tough_ken_ellis.sql
+++ apps/postybirb/src/migrations/0000_tough_ken_ellis.sql
@@ -2,9 +2,9 @@ CREATE TABLE `account` (
 	`id` text PRIMARY KEY NOT NULL,
 	`createdAt` text NOT NULL,
 	`updatedAt` text NOT NULL,
+	`groups` text NOT NULL,
 	`name` text NOT NULL,
-	`website` text NOT NULL,
-	`groups` text NOT NULL
+	`website` text NOT NULL
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `account_id_unique` ON `account` (`id`);--> statement-breakpoint
@@ -12,9 +12,9 @@ CREATE TABLE `directory-watcher` (
 	`id` text PRIMARY KEY NOT NULL,
 	`createdAt` text NOT NULL,
 	`updatedAt` text NOT NULL,
-	`path` text,
-	`importAction` text DEFAULT 'NEW_SUBMISSION' NOT NULL,
 	`templateId` text,
+	`importAction` text DEFAULT 'NEW_SUBMISSION' NOT NULL,
+	`path` text,
 	FOREIGN KEY (`templateId`) REFERENCES `submission`(`id`) ON UPDATE no action ON DELETE set null
 );
 --> statement-breakpoint
@@ -26,14 +26,28 @@ CREATE TABLE `file-buffer` (
 	`submissionFileId` text NOT NULL,
 	`buffer` blob NOT NULL,
 	`fileName` text NOT NULL,
+	`height` integer NOT NULL,
 	`mimeType` text NOT NULL,
 	`size` integer NOT NULL,
 	`width` integer NOT NULL,
-	`height` integer NOT NULL,
 	FOREIGN KEY (`submissionFileId`) REFERENCES `submission-file`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `file-buffer_id_unique` ON `file-buffer` (`id`);--> statement-breakpoint
+CREATE TABLE `notification` (
+	`id` text PRIMARY KEY NOT NULL,
+	`createdAt` text NOT NULL,
+	`updatedAt` text NOT NULL,
+	`title` text NOT NULL,
+	`message` text NOT NULL,
+	`tags` text NOT NULL,
+	`data` text NOT NULL,
+	`isRead` integer DEFAULT false NOT NULL,
+	`hasEmitted` integer DEFAULT false NOT NULL,
+	`type` text NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `notification_id_unique` ON `notification` (`id`);--> statement-breakpoint
 CREATE TABLE `post-queue` (
 	`id` text PRIMARY KEY NOT NULL,
 	`createdAt` text NOT NULL,
@@ -49,10 +63,10 @@ CREATE TABLE `post-record` (
 	`id` text PRIMARY KEY NOT NULL,
 	`createdAt` text NOT NULL,
 	`updatedAt` text NOT NULL,
-	`state` text DEFAULT 'PENDING' NOT NULL,
-	`resumeMode` text DEFAULT 'CONTINUE' NOT NULL,
 	`submissionId` text,
 	`completedAt` text,
+	`resumeMode` text DEFAULT 'CONTINUE' NOT NULL,
+	`state` text DEFAULT 'PENDING' NOT NULL,
 	FOREIGN KEY (`submissionId`) REFERENCES `submission`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
@@ -62,7 +76,7 @@ CREATE TABLE `settings` (
 	`createdAt` text NOT NULL,
 	`updatedAt` text NOT NULL,
 	`profile` text DEFAULT 'default' NOT NULL,
-	`settings` text DEFAULT '{"hiddenWebsites":[],"language":"en","allowAd":true,"queuePaused":false}' NOT NULL
+	`settings` text DEFAULT '{"hiddenWebsites":[],"language":"en","allowAd":true,"queuePaused":false,"desktopNotifications":{"enabled":true,"showOnPostSuccess":true,"showOnPostError":true,"showOnDirectoryWatcherError":true,"showOnDirectoryWatcherSuccess":true}}' NOT NULL
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `settings_id_unique` ON `settings` (`id`);--> statement-breakpoint
@@ -71,19 +85,19 @@ CREATE TABLE `submission-file` (
 	`id` text PRIMARY KEY NOT NULL,
 	`createdAt` text NOT NULL,
 	`updatedAt` text NOT NULL,
-	`fileName` text NOT NULL,
-	`hash` text NOT NULL,
-	`size` integer NOT NULL,
-	`mimeType` text NOT NULL,
-	`width` integer NOT NULL,
-	`height` integer NOT NULL,
-	`hasThumbnail` integer NOT NULL,
-	`hasAltFile` integer DEFAULT false NOT NULL,
-	`hasCustomThumbnail` integer DEFAULT false NOT NULL,
 	`submissionId` text NOT NULL,
 	`primaryFileId` text,
 	`thumbnailId` text,
 	`altFileId` text,
+	`fileName` text NOT NULL,
+	`hasAltFile` integer DEFAULT false NOT NULL,
+	`hasCustomThumbnail` integer DEFAULT false NOT NULL,
+	`hasThumbnail` integer NOT NULL,
+	`hash` text NOT NULL,
+	`height` integer NOT NULL,
+	`mimeType` text NOT NULL,
+	`size` integer NOT NULL,
+	`width` integer NOT NULL,
 	FOREIGN KEY (`submissionId`) REFERENCES `submission`(`id`) ON UPDATE no action ON DELETE cascade,
 	FOREIGN KEY (`primaryFileId`) REFERENCES `file-buffer`(`id`) ON UPDATE no action ON DELETE no action,
 	FOREIGN KEY (`thumbnailId`) REFERENCES `file-buffer`(`id`) ON UPDATE no action ON DELETE no action,
@@ -96,13 +110,13 @@ CREATE TABLE `submission` (
 	`createdAt` text NOT NULL,
 	`updatedAt` text NOT NULL,
 	`type` text NOT NULL,
+	`isArchived` integer DEFAULT false,
+	`isMultiSubmission` integer NOT NULL,
 	`isScheduled` integer NOT NULL,
 	`isTemplate` integer NOT NULL,
-	`isMultiSubmission` integer NOT NULL,
-	`isArchived` integer DEFAULT false,
-	`schedule` text NOT NULL,
 	`metadata` text NOT NULL,
-	`order` integer NOT NULL
+	`order` integer NOT NULL,
+	`schedule` text NOT NULL
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `submission_id_unique` ON `submission` (`id`);--> statement-breakpoint
@@ -110,8 +124,8 @@ CREATE TABLE `tag-converter` (
 	`id` text PRIMARY KEY NOT NULL,
 	`createdAt` text NOT NULL,
 	`updatedAt` text NOT NULL,
-	`tag` text NOT NULL,
-	`convertTo` text NOT NULL
+	`convertTo` text NOT NULL,
+	`tag` text NOT NULL
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `tag-converter_id_unique` ON `tag-converter` (`id`);--> statement-breakpoint
@@ -130,9 +144,9 @@ CREATE TABLE `user-specified-website-options` (
 	`id` text PRIMARY KEY NOT NULL,
 	`createdAt` text NOT NULL,
 	`updatedAt` text NOT NULL,
-	`type` text NOT NULL,
-	`options` text NOT NULL,
 	`accountId` text NOT NULL,
+	`options` text NOT NULL,
+	`type` text NOT NULL,
 	FOREIGN KEY (`accountId`) REFERENCES `account`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
@@ -140,10 +154,9 @@ CREATE UNIQUE INDEX `user-specified-website-options_id_unique` ON `user-specifie
 CREATE TABLE `website-data` (
 	`id` text PRIMARY KEY NOT NULL,
 	`createdAt` text NOT NULL,
-	`updatedAt` text NOT NULL,
 	`data` text DEFAULT '{}' NOT NULL,
-	`accountId` text NOT NULL,
-	FOREIGN KEY (`accountId`) REFERENCES `account`(`id`) ON UPDATE no action ON DELETE cascade
+	`updatedAt` text NOT NULL,
+	FOREIGN KEY (`id`) REFERENCES `account`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `website-data_id_unique` ON `website-data` (`id`);--> statement-breakpoint
@@ -151,10 +164,10 @@ CREATE TABLE `website-options` (
 	`id` text PRIMARY KEY NOT NULL,
 	`createdAt` text NOT NULL,
 	`updatedAt` text NOT NULL,
-	`data` text NOT NULL,
-	`isDefault` integer NOT NULL,
 	`accountId` text NOT NULL,
 	`submissionId` text NOT NULL,
+	`data` text NOT NULL,
+	`isDefault` integer NOT NULL,
 	FOREIGN KEY (`accountId`) REFERENCES `account`(`id`) ON UPDATE no action ON DELETE cascade,
 	FOREIGN KEY (`submissionId`) REFERENCES `submission`(`id`) ON UPDATE no action ON DELETE cascade
 );
@@ -164,15 +177,15 @@ CREATE TABLE `website-post-record` (
 	`id` text PRIMARY KEY NOT NULL,
 	`createdAt` text NOT NULL,
 	`updatedAt` text NOT NULL,
-	`postRecordId` text NOT NULL,
 	`accountId` text NOT NULL,
+	`postRecordId` text NOT NULL,
 	`completedAt` text,
 	`errors` text NOT NULL,
-	`postData` text NOT NULL,
 	`metadata` text NOT NULL,
+	`postData` text NOT NULL,
 	`postResponse` text,
-	FOREIGN KEY (`postRecordId`) REFERENCES `post-record`(`id`) ON UPDATE no action ON DELETE cascade,
-	FOREIGN KEY (`accountId`) REFERENCES `account`(`id`) ON UPDATE no action ON DELETE set null
+	FOREIGN KEY (`accountId`) REFERENCES `account`(`id`) ON UPDATE no action ON DELETE set null,
+	FOREIGN KEY (`postRecordId`) REFERENCES `post-record`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `website-post-record_id_unique` ON `website-post-record` (`id`);
