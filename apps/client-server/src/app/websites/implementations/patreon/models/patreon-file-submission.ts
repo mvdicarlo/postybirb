4b0d95d8983b7ca1b964e9e3f27b75629b7287ab
Leaftail <110915645+leaftail1880@users.noreply.github.com>
2026-01-31 10:27:52 +0300

    fix: add spaceReplacer option to tags field

--- apps/client-server/src/app/websites/implementations/patreon/models/patreon-file-submission.ts
+++ apps/client-server/src/app/websites/implementations/patreon/models/patreon-file-submission.ts
@@ -16,7 +16,8 @@ export class PatreonFileSubmission extends BaseWebsiteOptions {
   description: DescriptionValue;
 
   @TagField({
-    maxTagLength: 25
+    maxTagLength: 25,
+    spaceReplacer: " ",
   })
   tags: TagValue = DefaultTagValue();
 
