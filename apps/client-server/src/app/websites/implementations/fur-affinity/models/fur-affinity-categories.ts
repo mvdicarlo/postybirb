10ea9a8dac6f629030a0c59a5942d0bef209d302
leaftail1880 <110915645+leaftail1880@users.noreply.github.com>
2026-02-05 21:05:43 +0300

    feat(FA): add new categories

--- apps/client-server/src/app/websites/implementations/fur-affinity/models/fur-affinity-categories.ts
+++ apps/client-server/src/app/websites/implementations/fur-affinity/models/fur-affinity-categories.ts
@@ -1,21 +1,37 @@
 import { SelectOption } from '@postybirb/form-builder';
 
+// Run this script on https://www.furaffinity.net/submit/finalize/ to update categories
+// Array.prototype.map.call(document.querySelector("select[name='cat']"), (e) => ({value: e.value, label: e.label, parent: e.parentNode?.label})).reduce((e, c) => {  if (!c.parent) return e;  const f = e.find(e => e.label === c.parent) ?? (e[e.push({label: c.parent, items: []})-1]);  f.items.push({value: c.value, label: c.label});  return e;}, [])
+
 export const FurAffinityCategories: SelectOption[] = [
   {
     label: 'Visual Art',
     items: [
       { value: '1', label: 'All' },
+      { value: '34', label: '3D Models' },
       { value: '2', label: 'Artwork (Digital)' },
       { value: '3', label: 'Artwork (Traditional)' },
-      { value: '4', label: 'Cellshading' },
+      { value: '4', label: 'Cel Shading' },
       { value: '5', label: 'Crafting' },
       { value: '6', label: 'Designs' },
+      { value: '32', label: 'Food / Recipes' },
       { value: '8', label: 'Fursuiting' },
       { value: '9', label: 'Icons' },
       { value: '10', label: 'Mosaics' },
       { value: '11', label: 'Photography' },
-      { value: '32', label: 'Food / Recipes' },
+      { value: '36', label: 'Pixel Art' },
       { value: '12', label: 'Sculpting' },
+      { value: '35', label: 'Virtual Photography' },
+    ],
+  },
+  {
+    label: 'Animation & Media',
+    items: [
+      { value: '37', label: '2D Animation' },
+      { value: '38', label: '3D Animation' },
+      { value: '39', label: 'Pixel Animation' },
+      { value: '7', label: 'Flash' },
+      { value: '40', label: 'Interactive Media' },
     ],
   },
   {
