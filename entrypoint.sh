2335b4c07dfdfc36dbd9a553bdc908f66bfe98af
Leaftail <110915645+leaftail1880@users.noreply.github.com>
2026-04-07 14:22:06 +0300

    fix: remote log spam (#694)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/bash
 set -o pipefail
-xvfb-run --auto-servernum --server-args="-screen 0 1280x960x24" ./PostyBirb --headless --no-sandbox --port=8080 |& grep -v -E "ERROR:viz_main_impl\.cc\(183\)|ERROR:object_proxy\.cc\(576\)|ERROR:bus\.cc\(408\)"
+xvfb-run --auto-servernum --server-args="-screen 0 1280x960x24" ./PostyBirb --headless --no-sandbox --port=8080 |& grep -v -E "ERROR:viz_main_impl\.cc\(183\)|ERROR:object_proxy\.cc\(576\)|ERROR:bus\.cc\(408\)|ERROR:browser_main_loop\.cc\(276\)|ERROR:gles2_cmd_decoder_passthrough\.cc\(1094\)|ERROR:gl_utils\.cc\(431\)"
