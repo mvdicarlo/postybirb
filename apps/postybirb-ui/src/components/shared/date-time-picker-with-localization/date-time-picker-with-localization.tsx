7a9436c25df25c2a70aebc80d17b948d436ac06a
Leaftail <110915645+leaftail1880@users.noreply.github.com>
2026-05-26 14:08:32 +0300

    fix: date picker not working with 12h clock (#815)

--- apps/postybirb-ui/src/components/shared/date-time-picker-with-localization/date-time-picker-with-localization.tsx
+++ apps/postybirb-ui/src/components/shared/date-time-picker-with-localization/date-time-picker-with-localization.tsx
@@ -2,13 +2,14 @@ import { DateTimePicker, DateTimePickerProps, DayOfWeek } from '@mantine/dates';
 import dayjs from 'dayjs';
 import { useLocale } from '../../../hooks/use-locale';
 
-type Props = Omit<DateTimePickerProps, 'onChange'> & {
+type Props = Omit<DateTimePickerProps, 'onChange' | 'placeholder'> & {
   onChange(date: Date | null): void;
+  placeholder?: Date;
 };
 
 export function DateTimePickerWithLocalization(props: Props) {
   const { startOfWeek, dayjsDateTimeFormat, hourCycle } = useLocale();
-  const { onChange, valueFormat } = props;
+  const { onChange, valueFormat, placeholder } = props;
   const format = valueFormat ?? dayjsDateTimeFormat;
 
   return (
@@ -18,9 +19,10 @@ export function DateTimePickerWithLocalization(props: Props) {
       valueFormat={format}
       timePickerProps={{ format: hourCycle === 'h12' ? '12h' : '24h' }}
       {...props}
+      placeholder={placeholder ? dayjs(placeholder).format(format) : undefined}
       onChange={(value) => {
         if (value) {
-          onChange(dayjs(value, format).toDate());
+          onChange(new Date(value));
         } else {
           onChange(null);
         }
