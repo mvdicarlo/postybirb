bda052bba03279e90667f5f4e71ec07d7a6d9221
Leaftail <110915645+leaftail1880@users.noreply.github.com>
2026-06-17 14:46:30 +0300

    fix: docker build (#894)
    * fix: linux/amd64 turning into unknown/unknown
    
    * fix: linux/amd64 turning into unknown/unknown
    
    * ci: build.yml and entrypoint.sh
    
    * fix: docker: remove electron warning

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/bash
 set -o pipefail
-xvfb-run --auto-servernum --server-args="-screen 0 1280x960x24" ./PostyBirb --headless --no-sandbox |& grep -v -E "ERROR:viz_main_impl|ERROR:dbus/object_proxy|ERROR:dbus/bus|ERROR:content/browser/browser_main_loop|ERROR:gles2_cmd_decoder_passthrough|ERROR:gl_utils"
+xvfb-run --auto-servernum --server-args="-screen 0 1280x960x24" ./PostyBirb --headless --no-sandbox |& grep -v -E "ERROR:viz_main_impl|ERROR:dbus/object_proxy|ERROR:dbus/bus|ERROR:content/browser/browser_main_loop|ERROR:gles2_cmd_decoder_passthrough|ERROR:gl_utils|assertion 'G_IS_OBJECT \(object\)' failed"
