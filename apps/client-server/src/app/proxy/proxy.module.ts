7b4f32703e0f87fc1665d2968d9b1ff84de4b999
leRisen <lemmas.online@gmail.com>
2026-06-21 18:58:16 +0300

    fix(proxy): register AccountRepository in ProxyModule

--- apps/client-server/src/app/proxy/proxy.module.ts
+++ apps/client-server/src/app/proxy/proxy.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { AccountRepository } from '@postybirb/database';
 import { PacScriptService } from './pac-script.service';
 import { ProxyPacController } from './proxy-pac.controller';
 import { WebsiteDomainService } from './website-domain.service';
 
 @Module({
   controllers: [ProxyPacController],
-  providers: [WebsiteDomainService, PacScriptService],
+  providers: [
+    WebsiteDomainService,
+    PacScriptService,
+    {
+      provide: AccountRepository,
+      useFactory: () => new AccountRepository(),
+    },
+  ],
   exports: [WebsiteDomainService, PacScriptService],
 })
 export class ProxyModule {}
